e56a151d15fda8a82aa16976654b61495abe1710
xrc10 <ruochenx@gmail.com>
2025-01-16 18:20:00 +0800

    split two files

--- examples/VQA_with_mem0/run_app_stream.py
+++ examples/VQA_with_mem0/run_app_stream.py
@@ -66,19 +66,19 @@ task4_1 = simple_task(
 
 # Split answer generator into three tasks
 task5_0 = simple_task(
-    task_def_name="VQAAnswerGenerator",
+    task_def_name="VQAAnswerGeneratorStream",
     task_reference_name="answer_generator0",
     inputs={"user_instruction": task1.output("user_instruction")},
 )
 
 task5_1 = simple_task(
-    task_def_name="VQAAnswerGenerator",
+    task_def_name="VQAAnswerGeneratorStream",
     task_reference_name="answer_generator1",
     inputs={"user_instruction": task1.output("user_instruction")},
 )
 
 task5_2 = simple_task(
-    task_def_name="VQAAnswerGenerator",
+    task_def_name="VQAAnswerGeneratorStream",
     task_reference_name="answer_generator2",
     inputs={"user_instruction": task1.output("user_instruction")},
 )
