a8aeb25950dfe4bafdf26d2298c230588867a755
xrc10 <ruochenx@gmail.com>
2025-01-20 09:22:26 +0800

    update RAP lite example

--- examples/RAP/run_cli_simple_lite.py
+++ examples/RAP/run_cli_simple_lite.py
@@ -1,22 +1,43 @@
 # Import required modules and components
-import os
 from omagent_core.utils.container import container
 from omagent_core.engine.workflow.conductor_workflow import ConductorWorkflow
 from omagent_core.engine.workflow.task.simple_task import simple_task
 from pathlib import Path
 from omagent_core.utils.registry import registry
 from omagent_core.clients.devices.lite_version.cli import DefaultClient
-from omagent_core.engine.workflow.task.do_while_task import DoWhileTask
+from omagent_core.engine.workflow.task.set_variable_task import SetVariableTask
 from omagent_core.utils.logger import logging
+from omagent_core.engine.workflow.task.do_while_task import DoWhileTask
 from omagent_core.engine.worker.base import BaseWorker
 
-# Register a simple input worker similar to general_dnc example
+from agent.input_interface.input_interface import InputInterface
+
+SUPPORT_TASK = {
+    'math': "please input your math problem."
+}
+
 @registry.register_worker()
-class SimpleRAPInput(BaseWorker):
-    def _run(self, *args, **kwargs):
-        self.stm(self.workflow_instance_id)['task'] = 'math'
-        self.stm(self.workflow_instance_id)['data_input'] = "Henry and 3 of his friends order 7 pizzas for lunch. Each pizza is cut into 8 slices. If Henry and his friends want to share the pizzas equally, how many slices can each of them have?"
-        return {'data_input': "Henry and 3 of his friends order 7 pizzas for lunch. Each pizza is cut into 8 slices. If Henry and his friends want to share the pizzas equally, how many slices can each of them have?"}
+class SimpleInput(BaseWorker):
+    def _run(self, query, *args, **kwargs):
+        # Get task selection from user
+        task = input(f'\nWelcome to use OmAgent RAP Algorithm, please input the task you want to conduct. Choices: {list(SUPPORT_TASK.keys())} ')
+        
+        if task not in SUPPORT_TASK:
+            raise NotImplementedError
+        
+        self.stm(self.workflow_instance_id)['task'] = task 
+
+        # Use query parameter instead of reading new input
+        data_input = query
+        
+        # For debug
+        if data_input == 'x':
+            data_input = "Henry and 3 of his friends order 7 pizzas for lunch. Each pizza is cut into 8 slices. If Henry and his friends want to share the pizzas equally, how many slices can each of them have?"
+        
+        logging.info(f'data_input: {data_input}\n')
+        self.stm(self.workflow_instance_id)['data_input'] = data_input
+
+        return {'data_input': data_input}
 
 # Initialize logging
 logging.init_logger("omagent", "omagent", level="INFO")
@@ -27,13 +48,14 @@ CURRENT_PATH = Path(__file__).parents[0]
 # Import registered modules
 registry.import_module(project_path=CURRENT_PATH.joinpath('agent'))
 
-container.register_stm("RedisSTM")
+# Change STM to SharedMemSTM for lite version
+container.register_stm("SharedMemSTM")
 
-# Initialize workflow with lite_version=True
+# Initialize RAP workflow with lite_version=True
 workflow = ConductorWorkflow(name='RAP', lite_version=True)
 
 # Configure workflow tasks:
-client_input_task = simple_task(task_def_name=SimpleRAPInput, task_reference_name='input_task')
+client_input_task = simple_task(task_def_name=SimpleInput, task_reference_name='input_interface')
 
 task_selection = simple_task(task_def_name='Selection', task_reference_name='selection')
 
@@ -57,13 +79,11 @@ output_interface = simple_task(task_def_name='OutputInterface', task_reference_n
 # Configure workflow execution flow: 
 workflow >> client_input_task >> task_selection >> task_expansion >> task_simulation_preprocess >> simulation_loop >> task_back_propagation >> task_check >> output_interface
 
-# Register workflow with overwrite=True
+# Register workflow with overwrite=True for consistency with lite version
 workflow.register(overwrite=True)
 
 # Initialize and start CLI client with workflow configuration
 config_path = CURRENT_PATH.joinpath('configs')
-cli_client = DefaultClient(interactor=workflow, config_path=config_path, workers=[SimpleRAPInput()])
-cli_client.start_processor_with_input({"data_input": "Henry and 3 of his friends order 7 pizzas for lunch. Each pizza is cut into 8 slices. If Henry and his friends want to share the pizzas equally, how many slices can each of them have?"})
-
-# Uncomment to test with specific input
-#cli_client.start_processor_with_input({"query": "your test query here"})
+cli_client = DefaultClient(interactor=workflow, config_path=config_path, workers=[SimpleInput()])
+# Change start_interactor to start_interaction for lite version
+cli_client.start_interaction()
